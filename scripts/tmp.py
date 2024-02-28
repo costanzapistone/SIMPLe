e03fd20b07e4336f365ea409109b48bb7b763eb3
costanzapistone <giovanni.franzese94@gmail.com>
2024-02-28 18:32:34 +0100

    imp

--- scripts/tmp.py
+++ scripts/tmp.py
@@ -5,15 +5,20 @@ from scipy.io import loadmat
 from SIMPLe_bimanual.processing_functions import logvar, butter_bandpass, cov, whitening, csp, apply_mix
 
 # Define constants
-SUBJECT = 'd'
-MODEL = 'LDA' # Choose the model to use among: 'LDA', 'DT', 'RF', 'LR', 'NB' or 'SVM' 
+SUBJECT = 'g'
+MODEL = 'LR' # Choose the model to use among: 'LDA', 'DT', 'RF', 'LR', 'NB' or 'SVM' 
 MAT_FILE = f'/home/platonics/Documents/costanza_workspace/src/Robot-Control-by-EEG-with-ML/data/BCICIV_calib_ds1{SUBJECT}.mat'
 TRAINED_MODEL_FILE_PATH = f"/home/platonics/Documents/costanza_workspace/src/Robot-Control-by-EEG-with-ML/code/classification/Subject_{SUBJECT}_lab/Trained_Models/{MODEL}_model.pkl"
 W_MATRIX_FILE_PATH = f"/home/platonics/Documents/costanza_workspace/src/Robot-Control-by-EEG-with-ML/code/classification/Subject_{SUBJECT}_lab/Trained_Models/CSP_matrix_W.pkl"
+TRAIN_PERCENTAGE = 0.6
 
 # Load the classifier from the file
 with open (TRAINED_MODEL_FILE_PATH, 'rb') as file:
     classifier = pickle.load(file)
+
+# Load the W matrix from the file
+with open (W_MATRIX_FILE_PATH, 'rb') as file:
+    W = pickle.load(file)
 
 # Load the .mat file
 EEG_data = loadmat(MAT_FILE, struct_as_record = True)
@@ -42,7 +47,7 @@ for cl, code in zip(cl_lab, np.unique(event_codes)):
 trials_filt = {cl1: butter_bandpass(trials[cl1], 8, 30, sfreq, nsamples),
                 cl2: butter_bandpass(trials[cl2], 8, 30, sfreq, nsamples)}
 
-train_percentage = 0.7
+train_percentage = TRAIN_PERCENTAGE
 # Calculate the number of trials for each class the above percentage boils down to
 ntrain_l = int(trials_filt[cl1].shape[2] * train_percentage)
 ntrain_r = int(trials_filt[cl2].shape[2] * train_percentage)
@@ -55,10 +60,6 @@ train = {cl1: trials_filt[cl1][:,:,:ntrain_l],
 
 test = {cl1: trials_filt[cl1][:,:,ntrain_l:],
         cl2: trials_filt[cl2][:,:,ntrain_r:]}
-
-# Load the W matrix from the file
-with open (W_MATRIX_FILE_PATH, 'rb') as file:
-    W = pickle.load(file)
 
 # Apply the CSP algorithm
 train[cl1] = apply_mix(W, train[cl1], nchannels, nsamples)
@@ -79,20 +80,23 @@ train[cl2] = logvar(train[cl2])
 test[cl1] = logvar(test[cl1])
 test[cl2] = logvar(test[cl2])
 
-#%% Get a random sample from the test set of the class 1 - right
+#%% Get a random sample from the test set of the class1
 random_index = np.random.choice(test[cl1].shape[1])
 sample = test[cl1][:, random_index]
 print(sample.shape)
 print(sample.reshape(-1,1).shape)
 #%% Get the prediction and the probability of the prediction
 y_pred = classifier.predict(sample.reshape(1,-1))
-pred_proba = classifier.predict_proba(sample.reshape(1,-1))
 print(y_pred)
-if y_pred == 0:
+if y_pred == 1:
     print('Predicted Movement: Right')
     # Move the right robot
 else:
     print('Predicted Movement: Left')
     # Move the left robot
 
+# %%
+
+pred_proba = classifier.predict_proba(sample.reshape(1,-1))
+print(pred_proba)
 # %%
